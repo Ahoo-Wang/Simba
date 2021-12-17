4d4031c21a4dbe95ff65089ccbeada87e9427178
Ahoo Wang <ahoowang@qq.com>
2021-12-18 00:34:15 +0800

    optimize mutex_guard.lua

--- simba-redis/src/main/resources/mutex_guard.lua
+++ simba-redis/src/main/resources/mutex_guard.lua
@@ -4,9 +4,8 @@ local contenderId = ARGV[1];
 local transition = ARGV[2];
 local mutexKey = 'simba:' .. mutex;
 
--- 1. 判断当前持有互斥体的是否为自己
-if redis.call("get", mutexKey) ~= contenderId then
-    -- 获取当前持有者 & ttl
+-- 获取当前持有者 & ttl
+local function getCurrentOwner(mutexKey)
     local ownerId = redis.call("get", mutexKey)
     if ownerId then
         local ttl = redis.call("pttl", mutexKey)
@@ -15,11 +14,13 @@ if redis.call("get", mutexKey) ~= contenderId then
     return '@@';
 end
 
+-- 1. 判断当前持有互斥体的是否为自己
+if redis.call("get", mutexKey) ~= contenderId then
+    return getCurrentOwner(mutexKey)
+end
+
 if redis.call("set", mutexKey, contenderId, 'xx', 'px', transition) then
     return contenderId .. '@@' .. transition;
 else
-    -- 获取当前持有者 & ttl
-    local ownerId = redis.call("get", mutexKey)
-    local ttl = redis.call("pttl", mutexKey)
-    return ownerId .. '@@' .. ttl;
+    return getCurrentOwner(mutexKey)
 end
