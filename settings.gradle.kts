c30d92d2cd6799f60eb70617e745600a18696721
Ahoo Wang <ahoowang@qq.com>
2023-07-01 21:15:22 +0800

    refactor: dependency management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,13 +24,3 @@ include(":simba-test")
 include(":simba-example")
 
 include(":code-coverage-report")
-
-pluginManagement {
-    plugins {
-        id("io.gitlab.arturbosch.detekt") version "1.23.0" apply false
-        kotlin("jvm") version "1.8.22" apply false
-        kotlin("plugin.spring") version "1.8.22" apply false
-        id("org.jetbrains.dokka") version "1.8.20" apply false
-        id("io.github.gradle-nexus.publish-plugin") version "1.3.0" apply false
-    }
-}
