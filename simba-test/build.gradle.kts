ea580d6e4cf72d91355cd1d09ae51006b4a8e6e3
Ahoo Wang <ahoowang@qq.com>
2026-08-18 11:28:06 +0800

    test: expose fluent-assert to shared tck

--- simba-test/build.gradle.kts
+++ simba-test/build.gradle.kts
@@ -2,6 +2,8 @@ description = "The Technology Compatibility Kit"
 
 dependencies {
     api(project(":simba-core"))
+    api(platform(libs.fluent.assert.bom))
+    api("me.ahoo.test:fluent-assert-core")
     api("org.hamcrest:hamcrest")
     implementation("org.junit.jupiter:junit-jupiter-api")
 }
